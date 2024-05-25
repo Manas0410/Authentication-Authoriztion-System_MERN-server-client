23a834ba8eb93f3e4c2e3c8c2ebf1dec4bac5bd5
Manas0410 <manasshrivastava0410@gmail.com>
2024-05-26 02:04:33 +0530

    authProvider

---
+++ client/src/authContext.tsx
@@ -0,0 +1,48 @@
+import React, {
+  createContext,
+  useState,
+  ReactNode,
+  Dispatch,
+  SetStateAction,
+} from "react";
+
+// Define the user type
+interface User {
+  name: string;
+  email: string;
+  role: string;
+}
+
+// Define the context type
+interface UserContextType {
+  user: User | null;
+  accessToken: string | null;
+  setUser: Dispatch<SetStateAction<User | null>>;
+  setAccessToken: Dispatch<SetStateAction<string | null>>;
+}
+
+// Create the context with a default value of null
+const UserContext = createContext<UserContextType | null>(null);
+
+interface UserContextProviderProps {
+  children: ReactNode;
+}
+
+const UserContextProvider: React.FC<UserContextProviderProps> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  return (
+    <UserContext.Provider
+      value={{ user, accessToken, setUser, setAccessToken }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserContextProvider };
+
+// axios.get("ur_url_here/",data,{withCredentials:true})
